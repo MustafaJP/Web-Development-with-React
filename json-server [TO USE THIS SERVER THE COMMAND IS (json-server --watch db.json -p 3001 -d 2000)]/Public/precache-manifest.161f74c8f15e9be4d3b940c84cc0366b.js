9d8f2c0475e764048f2435f6b92a3a473c3c4c73
MustafaJP <mustafapoonawala@yahoo.com>
2020-07-23 15:54:57 +0800

    Course complete

---
+++ json-server [TO USE THIS SERVER THE COMMAND IS (json-server --watch db.json -p 3001 -d 2000)]/Public/precache-manifest.161f74c8f15e9be4d3b940c84cc0366b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c36573275a09428c8b7a21102ed5c80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9d8586f6a517f0ee8e5",
+    "url": "/static/css/2.7f0a60d8.chunk.css"
+  },
+  {
+    "revision": "bda48ee189a9aae741be",
+    "url": "/static/css/main.bc2a2813.chunk.css"
+  },
+  {
+    "revision": "f9d8586f6a517f0ee8e5",
+    "url": "/static/js/2.1c502e23.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.1c502e23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda48ee189a9aae741be",
+    "url": "/static/js/main.cc3ccbb2.chunk.js"
+  },
+  {
+    "revision": "84926f692196015996db",
+    "url": "/static/js/runtime-main.c71f3b90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
